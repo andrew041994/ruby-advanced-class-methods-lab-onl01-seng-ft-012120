daaaaa93f6e5831c4ee57a79c08089962ae224aa
Alehandro Persaud <andrew041994@gmail.com>
2020-01-31 04:29:18 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,10 +34,11 @@ class Song
   end
 
   def self.find_or_create_by_name(title)
-
+      if @@all.include?(title)
       self.find_by_name(title)
-
+    else
       self.create_by_name(title)
+    end
   end
 
 
