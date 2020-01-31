0d7e5bab8a257bdcc5ec9ffd1a3194193eef28aa
Alehandro Persaud <andrew041994@gmail.com>
2020-01-31 05:04:04 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -45,7 +45,7 @@ class Song
   def self.new_from_filename(filename)
 
       song_data = filename.split("-")
-      song_name = song_data[1].split(".").delete_at[1]
+      song_name = song_data[1].split(".")
       puts song_name
 
   end
