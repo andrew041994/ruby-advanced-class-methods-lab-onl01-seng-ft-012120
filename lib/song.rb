315a9e465f5ee1c4202f3d0ae70129f0f85dc99f
Alehandro Persaud <andrew041994@gmail.com>
2020-01-31 03:45:22 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,5 @@
+require "pry"
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -9,5 +11,22 @@ class Song
   def save
     self.class.all << self
   end
+
+  def self.create
+    @@all << song = Song.new
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name= name
+
+  end
+
+
+
+
+
+
 
 end
