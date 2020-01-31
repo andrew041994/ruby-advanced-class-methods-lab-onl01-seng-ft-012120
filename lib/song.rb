b679fb7dbf9eabab2f1b8a7f3456cf0a6bb684a9
Alehandro Persaud <andrew041994@gmail.com>
2020-01-31 04:58:40 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -47,7 +47,7 @@ class Song
       song_data = filename.split("-")
       song_name = song_data[1].split(".")
       song_name[1].delete(".mp3")
-      puts song name
+      puts song_name
   end
 
 
