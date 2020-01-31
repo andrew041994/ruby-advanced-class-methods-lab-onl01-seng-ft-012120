eee7a7f0165660fb78cc52d255b31dcfbfd8bbd1
Alehandro Persaud <andrew041994@gmail.com>
2020-01-31 04:27:55 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -35,9 +35,9 @@ class Song
 
   def self.find_or_create_by_name(title)
 
-      self.find_by_name
+      self.find_by_name(title)
 
-      self.create_by_name
+      self.create_by_name(title)
   end
 
 
